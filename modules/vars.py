7d62a8aee86b23279d407419333a55077ea309b8
Newworld528 <bots4657@gmail.com>
2024-06-02 15:00:19 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "20600758"
+api_hash = "a880f40605f52c3da33c63b3c4d939a9"
+bot_token = "7327091338:AAGoHNOEx1Zf-zlS1u7gbAM3PjzLIv8sGdA"
